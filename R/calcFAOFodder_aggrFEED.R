13b69d7698eb5a4b607c907bebdbe77bd6e38209
Patrick Rein <patrick.rein@pik-potsdam.de>
2026-01-23 10:49:04 +0100

    Adds another comment describing why a readFAO, fodder call can not be updated

--- R/calcFAOFodder_aggrFEED.R
+++ R/calcFAOFodder_aggrFEED.R
@@ -21,6 +21,11 @@
 
 calcFAOFodder_aggrFEED <- function() {
   
+  # The following call can not be updated to FAO_online for two reasons:
+  # 1.) Currently FAO_online does not support the Fodder subtype
+  # 2.) FAO and FAO_online have different categories, thus this could potentially
+  #     break users of the resulting input data.
+  # -- patrickr, 2026-01-23
   data <- readSource("FAO", "Fodder")
   # load sectoral mapping
   aggregation <- toolGetMapping(type = "sectoral",
